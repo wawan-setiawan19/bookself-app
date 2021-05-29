1e4d636fc2bc1e4beb76c61cab31da933b959bbe
onepro19 <w.settiawan@gmail.com>
2021-05-29 23:07:41 +0700

    add storage local

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const titleElement = document.querySelector("#inputBookTitle");
 const authorElement = document.querySelector("#inputBookAuthor");
 const yearElement = document.querySelector("#inputBookYear");
 const completeElement = document.querySelector("#inputBookIsComplete");
-const validateYearElement = document.querySelector('.validasi');
+const validateYearElement = document.querySelector(".validasi");
 
 const date = new Date();
 
@@ -16,27 +16,44 @@ formElement.addEventListener("submit", (e) => {
     completeElement.checked = false;
 });
 
-const showErrorValidasi = (value) =>{
-    value !== true ?(validateYearElement.style.visibility = "hidden")
+const showErrorValidasi = (value) => {
+    value !== true
+        ? (validateYearElement.style.visibility = "hidden")
         : (validateYearElement.style.visibility = "visible");
-}
+};
 
-const cekDigitTahun = (digit) =>{
-    digit === 4
-        ? showErrorValidasi(false)
-        : showErrorValidasi(true)
-    if(digit < 4) validateYearElement.innerText = 'digit tahun kurang dari 4';
-    if(digit === 0) showErrorValidasi(false);
-}
+// Validasi tahun
+const cekDigitTahun = (digit) => {
+    digit === 4 ? showErrorValidasi(false) : showErrorValidasi(true);
+    if (digit < 4) validateYearElement.innerText = "digit tahun kurang dari 4";
+    if (digit === 0) showErrorValidasi(false);
+};
 
-const cekTahun = (tahun) =>{
-    if(tahun > date.getFullYear()){
+const cekTahun = (tahun) => {
+    if (tahun > date.getFullYear()) {
         showErrorValidasi(true);
-        validateYearElement.innerText = `kan masih tahun ${date.getFullYear()}`
+        validateYearElement.innerText = `kan masih tahun ${date.getFullYear()}`;
     }
-}
+};
 
-yearElement.addEventListener('keyup',(e)=>{
+yearElement.addEventListener("keyup", (e) => {
     cekDigitTahun(e.target.value.length);
     cekTahun(e.target.value);
-})
+});
+
+// bagian storage
+const bookData = "BOOK_DATA";
+
+// cek storage
+const cekStorage = () => {
+    return typeof Storage !== "undefined";
+};
+window.addEventListener("load", () => {
+    if (cekStorage()) {
+        if (localStorage.getItem(bookData) === null) {
+            localStorage.setItem(bookData, "");
+        }
+    } else {
+        console.log("storage tidak tersedia pada browser ini");
+    }
+});
