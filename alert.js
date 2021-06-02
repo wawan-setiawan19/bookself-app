c28a1ad36e3c08175e0e4aedbc33ad43ab59d8d4
wawan-setiawan19 <wawan19@upi.edu>
2021-06-02 17:37:05 +0700

    edit layout

--- alert.js
+++ alert.js
@@ -1,12 +1,16 @@
 const alertElement = document.querySelector(".alert");
 const dangerElement = document.querySelector(".danger-container");
 const showAlert = (message, type) => {
-  type === "danger"
-    ? alertElement.classList.add("red")
-    : alertElement.classList.add("green");
+  if (type === "danger") {
+    alertElement.classList.add("red");
+    alertElement.classList.remove("green");
+  } else {
+    alertElement.classList.remove("red");
+    alertElement.classList.add("green");
+  }
   alertElement.style.visibility = "visible";
   alertElement.innerText = message;
-  alertElement.style.top = `${window.scrollY+100}px`;
+  alertElement.style.top = `${window.scrollY + 100}px`;
   setTimeout(() => {
     alertElement.style.visibility = "hidden";
   }, 2000);
